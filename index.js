d0fcb216cf994937729443469795d61fdfbbee18
f-bernardo-09 <bernardoalen09@gmail.com>
2026-03-11 16:02:07 -0300

    Add CREATE e READ no index

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 import Usuario from "./model/usuario.js";
+
+// CREATE
+await Usuario.create({
+  email: "joao@gmail.com",
+  nome: "João",
+})
+  .then((usuario) => console.log("Usuário criado com sucesso: ", usuario))
+  .catch((error) => console.log("Erro ao criar usuário: ", error));
+
+// READ
+console.log(await Usuario.findAll());
+
+// UPDATE
