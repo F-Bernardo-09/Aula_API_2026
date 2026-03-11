b79fdcca047c02529781fcdcb7f96f8b4f9beaa4
f-bernardo-09 <bernardoalen09@gmail.com>
2026-03-11 16:27:57 -0300

    Add UPDATE e DELETE no index

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ await Usuario.create({
   .then((usuario) => console.log("Usuário criado com sucesso: ", usuario))
   .catch((error) => console.log("Erro ao criar usuário: ", error));
 
+const usuarion = await Usuario.findByPk("joao@gmail.com");
+console.log(usuario);
+
 // READ
 console.log(await Usuario.findAll());
 
 // UPDATE
+usuario.set({
+  nome: "João Silva",
+});
+await usuario.save();
+
+// DELETE
+const usuario = await Usuario.findByPk("joao@gmail.com");
+await usuario.destroy();
